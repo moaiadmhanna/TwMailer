535a24c10711ca930ee5c312514fe1b15917b611
Mina Mansour <mansourmina10@gmail.com>
2024-10-17 19:14:08 +0200

    Saving File completed

--- server/twmailer-server.c
+++ server/twmailer-server.c
@@ -5,37 +5,54 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>  
+#include <sys/types.h> 
+#include <dirent.h>
+#include <errno.h>
 #define BUFFER_SIZE 50
+
+
+typedef struct{
+    char *sender;
+    char *receiver;
+    char *subject;
+    char *message;
+} Mail_Body;
+
 
 void create_socket(int *sfd);
 void setup_socket(int sfd, struct sockaddr_in *serveraddr);
 void trim(char *input);
 void listening(int sfd);
-int communication(int consfd, char *buffer, int buffersize);
+int communication(int consfd, char *buffer, int buffersize,char* mail_dir);
 void accept_client(int sfd, int *peersoc);
-void receive_message(int peersoc, char *buffer, int buflen);
-void handle_send_message(char *buffer,int consfd);
-void handle_list_message(char *buffer,int consfd);
-void handle_del_message(char *buffer,int consfd);
-void handle_read_message(char *buffer,int consfd);
-void handle_quit_message(char *buffer, int consfd);
+void receive_message(int peersoc, char *buffer, int buflen, char* mail_dir);
+void handle_send_client(char *buffer,int consfd, char* mail_dir);
+void handle_list_message(char *buffer,int consfd, char* mail_dir);
+void handle_del_message(char *buffer,int consfd, char* mail_dir);
+void handle_read_message(char *buffer,int consfd, char* mail_dir);
+void handle_quit_message(char *buffer, int consfd, char* mail_dir);
+void send_client(char *message, int consfd);
+void save_message(Mail_Body mail_body, int consfd, char* mail_dir);
 
 typedef struct {
     char* name;
-    void (*func)(char*, int);
+    void (*func)(char*, int, char*);
 } option;
 
 option options[] = {
-    {"SEND", handle_send_message},
+    {"SEND", handle_send_client},
     {"LIST", handle_list_message},
     {"DEL", handle_del_message},
     {"READ", handle_read_message},
     {"QUIT", handle_quit_message}
 };
 
+
+
 int main(const int argc, char *argv[]){
-    char* ip = argv[1];
-    char* port = argv[2];
+    char* port = argv[1];
+    char* mail_dir = argv[2];
 
     int sfd;
     create_socket(&sfd);
@@ -46,7 +63,7 @@ int main(const int argc, char *argv[]){
             .sin_addr.s_addr = INADDR_ANY
     };
 
-    inet_aton(ip, &serveraddr.sin_addr);
+    inet_aton("127.0.0.3", &serveraddr.sin_addr);
     setup_socket(sfd, &serveraddr);
     listening(sfd);
 
@@ -55,7 +72,7 @@ int main(const int argc, char *argv[]){
     while(1)
     {
         accept_client(sfd, &peersoc); 
-        receive_message(peersoc,buffer,BUFFER_SIZE); 
+        receive_message(peersoc,buffer,BUFFER_SIZE, mail_dir); 
     }
     close(sfd); 
     return 0;
@@ -89,21 +106,21 @@ void listening(int sfd){
         exit(EXIT_FAILURE);
     }
 }
-int communication(int consfd, char *buffer, int buffersize)
+int communication(int consfd, char *buffer, int buffersize, char* mail_dir)
 {
     memset(buffer, 0, buffersize);
-    int size = 0;
-    size = recv(consfd, &buffer[0],buffersize,0);
+    int size = recv(consfd, &buffer[0],buffersize,0);
     if(size == -1) {
         printf("cannot receive due to %d \n", errno);
         exit(EXIT_FAILURE);
     }
+
     char option[BUFFER_SIZE];
     strcpy(option,buffer);
     trim(option);
     for(int i = 0; i < sizeof(options)/sizeof(options[0]); i++){
         if(strcasecmp(option, options[i].name) == 0){
-            options[i].func(buffer, consfd);
+            options[i].func(buffer, consfd, mail_dir);
         }
     }
     return 0;
@@ -120,9 +137,9 @@ void accept_client(int sfd, int* peersoc){
     printf("Accepted with filedescriptor of requesting socket %d \n", *peersoc);
 }
 
-void receive_message(int peersoc, char* buffer, int buflen){
+void receive_message(int peersoc, char* buffer, int buflen, char* mail_dir){
     buffer[0] = '\0';
-    while(communication(peersoc,buffer,buflen) == 0);
+    while(communication(peersoc,buffer,buflen, mail_dir) == 0);
 }
 
 void trim(char *input)
@@ -136,8 +153,22 @@ void trim(char *input)
         input[len - 2] = '\0';
     }
 }
-void handle_send_message(char* buffer, int consfd)
+
+void tokenize_message(char* buffer, char** send_obj) {
+    char *tokens = strtok(buffer, "\n");
+    trim(tokens);
+    if (tokens == NULL)
+        exit(EXIT_FAILURE);
+    for (int i = 0; i < 5 && tokens != NULL; i++) {
+        trim(tokens);
+        send_obj[i] = tokens;
+        tokens = strtok(NULL, "\n");
+    }
+}
+
+void handle_send_client(char* buffer, int consfd, char* mail_dir)
 {
+    // Receive Message
     int total = 0; 
     int size = 0;
     while((size = recv(consfd, &buffer[total],BUFFER_SIZE,0)) > 0) {
@@ -146,58 +177,104 @@ void handle_send_message(char* buffer, int consfd)
             break; 
         }
     };
-    char *tokens = strtok(buffer, "\n");
-    trim(tokens);
-    if (tokens == NULL)
-    {
-        exit(EXIT_FAILURE);
-    } 
+
+    // Tokenize Message
     char *send_obj[5]  = {'\0'};
-    printf("---------- SEND ----------- \n");
-    for(int i = 0; i < 5; i++){
-        if(strcmp(tokens,".") != 0)
-            send_obj[i] = tokens;
-        tokens = strtok(NULL, "\n"); 
-        if(tokens == NULL){
-            break;
-        }
-        trim(tokens);
-    }
-    for(int i = 0; i < 4; i++)
+    tokenize_message(buffer, send_obj);
+   
+    for(int i = 0; send_obj[i] != NULL; i++)
     {
         if(send_obj[i] == NULL)
         {
-            printf("Message Object is not Complete\n");
-            char* message = "Invalid Argument\n";
-            send(consfd,message,strlen(message),0);
+            send_client("Invalid Argument", consfd);
             return;
         }
     }
-    printf("Sender : %s \n", send_obj[0]);
-    printf("Receiver : %s \n", send_obj[1]);
-    printf("Subject : %s \n", send_obj[2]);
-    printf("Message : %s \n", send_obj[3]);
-    char* message = "OK Message sended\n";
-    send(consfd,message,strlen(message),0);
+    Mail_Body mail_body = {
+        sender: send_obj[0],
+        receiver: send_obj[1],
+        subject: send_obj[2],
+        message: send_obj[3]
+    };
+    save_message(mail_body, consfd, mail_dir);
+    send_client("Message sended!", consfd);
 }
 
-void handle_quit_message(char* buffer, int consfd){
-    printf("end connection with client. \n");
+void send_client(char* message, int consfd) {
+    int message_length = strlen(message);
+    char *new = malloc(message_length + 1 + 1);
+
+    strcpy(new, message);
+    new[message_length] = '\n';
+    new[message_length + 1] = '\0';
+
+    send(consfd, new, strlen(new), 0);
+    free(new);
+}
+void handle_quit_message(char* buffer, int consfd, char* mail_dir){
+    printf("End connection with client. \n");
     close(consfd);
     exit(0);
 }
 
-void handle_list_message(char* buffer, int consfd){
-    printf("Hello list usernames \n");
+void handle_list_message(char* buffer, int consfd, char* mail_dir){
+   send_client("User lust", consfd);
 
 }
 
-void handle_del_message(char* buffer, int consfd){
+void handle_del_message(char* buffer, int consfd, char* mail_dir){
     printf("Del messages \n");
 
 }
 
-void handle_read_message(char* buffer, int consfd){
+void handle_read_message(char* buffer, int consfd, char* mail_dir){
     printf("Read messages \n");
 
+}
+
+
+void save_message(Mail_Body mail_body, int consfd,  char* mail_dir) {
+    FILE *fptr;
+   
+    int root_dir_len = strlen(mail_dir);
+    int dir_len = strlen(mail_body.receiver);
+    int path_sender_len = strlen(mail_body.sender);
+    int path_sub_len = strlen(mail_body.subject);
+    int type_len = strlen(".txt");
+    int sep_len = strlen("-");
+
+    // Allocate memory for the receiver directory name
+    char *dirname = malloc(dir_len + 1);
+    strcpy(dirname, mail_body.receiver);
+
+    // Full path for the receiver directory
+    char *receiver_path = malloc(root_dir_len + dir_len + 2); // +2 for slash and null terminator
+    sprintf(receiver_path, "%s/%s", mail_dir, dirname);
+
+    // Check if the receiver's directory exists, create it if not
+    if (access(receiver_path, F_OK) == -1) {
+        mkdir(receiver_path, 0777);
+    }
+
+    // Full file path
+    char *path = malloc(root_dir_len + dir_len + path_sender_len + sep_len + path_sub_len + type_len + 4); // +4 for slashes and null terminator
+    sprintf(path, "%s/%s/%s-%s.txt", mail_dir, dirname, mail_body.sender, mail_body.subject);
+
+    // When same file exists
+    int counter = 1;
+    while (access(path, F_OK) != -1) { 
+        sprintf(path, "%s/%s/%s-%s(%d).txt", mail_dir, dirname, mail_body.sender, mail_body.subject, counter);
+        counter++;
+    }
+
+    fptr = fopen(path, "w");
+
+    // Write the message to the file
+    fprintf(fptr, "%s", mail_body.message);
+
+    // Close the file
+    fclose(fptr);
+    free(path);
+    free(dirname);
+    free(receiver_path);
 }
